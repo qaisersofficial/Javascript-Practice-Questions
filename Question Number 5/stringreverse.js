e73df44fb40fa10c2d9a2c56647767d5183570eb
QAISER ALI <qaisers.official@gmail.com>
2024-08-10 11:40:06 +0500

    Reversing a sting using for loop

---
+++ Question Number 5/stringreverse.js
@@ -0,0 +1,9 @@
+//Reverse a string by using for loop
+function revstr(Any_String){
+    let result_string="";
+    for(let i=Any_String.length-1;i>=0; i--){
+        result_string= result_string+Any_String[i];
+    }
+    return result_string;
+}
+console.log(revstr("Qaiser Ali"))

e276ed703d664683ad0efda618d7181b86731efb
QAISER ALI <qaisers.official@gmail.com>
2024-08-10 11:56:18 +0500

    try it with another string

--- Question Number 5/stringreverse.js
+++ Question Number 5/stringreverse.js
@@ -2,8 +2,8 @@
 function revstr(Any_String){
     let result_string="";
     for(let i=Any_String.length-1;i>=0; i--){
-        result_string= result_string+Any_String[i];
+        result_string= result_string+Any_String[i]; // or result_string+= Any_String[i];
     }
     return result_string;
 }
-console.log(revstr("Qaiser Ali"));
+console.log(revstr("My name is Qaiser Ali, i am new to programming with javascript"));

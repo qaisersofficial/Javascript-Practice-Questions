72f365492b9ac0219f8fde27553959f85c675e0b
QAISER ALI <qaisers.official@gmail.com>
2024-08-11 12:15:49 +0500

    this program will print in every next line

--- Question Number 6/pattern.js
+++ Question Number 6/pattern.js
@@ -3,5 +3,5 @@ for (let i=1;i<=5;i++){
     for (let j=1;j<=i;j++){
         row+="*";
    }
-   console.log(row);
+   console.log(row); //but it will print in next line commit message does not contain next line tag hahaha
 }

7a4707d36356013b7b223d658f8e49b010ed015c
QAISER ALI <qaisers.official@gmail.com>
2024-08-11 14:48:58 +0500

    program is created

---
+++ Question Number 8/pattern3.js
@@ -0,0 +1,7 @@
+for (let i=1;i<=5;i++){
+    let row="";
+    for (let j=1;j<=i;j++){
+        row+=i;         //change j to i to print repectively counting if you have any problem to understand it you can check Question number 7
+   }
+   console.log(row); 
+}

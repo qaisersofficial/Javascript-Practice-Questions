8a331a1c71fba9782c8bd92ac165bcee8b6a5187
QAISER ALI <qaisers.official@gmail.com>
2024-08-11 23:29:00 +0500

    inner loop is created that will print *

--- Question Number 9/pattern4.js
+++ Question Number 9/pattern4.js
@@ -1,7 +1,11 @@
-let i,j;
+let i,j,k;
  for( i = 1; i <= 5; i++){
+    let row="";
     for( j = 1; j <= 5-i ; j++){
         row+=" ";
         
+    }
+    for( k=1; k<=i;k++){
+       row+="*"
     }
 }

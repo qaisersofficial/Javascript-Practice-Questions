57a8b10565bf93726c93df628b24978efacaeaa5
QAISER ALI <qaisers.official@gmail.com>
2024-08-12 22:55:42 +0500

    first verify the validity of entery of user

--- Question Number 11/code.js
+++ Question Number 11/code.js
@@ -1,4 +1,7 @@
 let input;
 do{
     input = +prompt("Please enter number greater than 100");
+    if(isNaN(input)){
+        alert("Please enter a valid number")
+     }
 }while(input <= 100 && input)

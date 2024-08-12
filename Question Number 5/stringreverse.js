72b86d8b1840501649b04499b49c7de5eb0737f2
QAISER ALI <qaisers.official@gmail.com>
2024-08-12 10:37:55 +0500

    try with different method

--- Question Number 5/stringreverse.js
+++ Question Number 5/stringreverse.js
@@ -13,11 +13,13 @@
 
 // split() ,  reverse(),  join()
 
-function revstr (Any_string){
-    let split_string = Any_string.split("");
-    let Rev_array = split_string.reverse();
-    let join_Array = Rev_array.join(""); 
+// function revstr (Any_string){
+//     let split_string = Any_string.split("");
+//     let Rev_array = split_string.reverse();
+//     let join_Array = Rev_array.join(""); 
 
-    return join_Array;
-}
-console.log(revstr("i am currently practing javascript Questions"));
+//     return join_Array;
+// }
+// console.log(revstr("i am currently practing javascript Questions"));
+
+// chaining these three function together

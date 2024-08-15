fb2d923e0475d8c3d31ce89463af99a6d36184a2
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 11:16:00 +0500

    trying console properties

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -1,6 +1,7 @@
 const name = "Qaiser Ali";
 const age =  "23";
 const city = "Gujrat";
-    console.log(name);
-    console.log(age);
-    console.log(city);
+    // console.log(name);
+    // console.log(age);
+    // console.log(city);
+    console.log(`My name is ${name}, \nMy age is ${age}, \nMy city is ${city}.`)

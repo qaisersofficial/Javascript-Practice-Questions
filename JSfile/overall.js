ac1494d938e025a5a9d9eac35aa086a521a99741
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 12:01:34 +0500

    ?? operator in JavaScript is called the nullish coalescing operator. It returns the right-hand side operand when the left-hand side operand is null or undefined

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -64,8 +64,11 @@
 
     //> opertor
 
-     console.log ( 10 > 5); //true
-     console.log ( "10" > 5); //true
-     console.log ( 10 > "5"); //true
-     console.log ( "10" > "5"); //false
+    //  console.log ( 10 > 5); //true
+    //  console.log ( "10" > 5); //true
+    //  console.log ( 10 > "5"); //true
+    //  console.log ( "10" > "5"); //false
 
+    // ??
+
+         console.log ( 10 ?? 5); //10

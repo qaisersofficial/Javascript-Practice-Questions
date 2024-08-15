e8a2d8754b62603ee1034e8c31bfb6415278c1ae
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 14:52:39 +0500

    practice question

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -207,3 +207,17 @@
 //     let SL_VAR = info( undefined,  "Soban Liaqat", 21); 
 //     console.log(SL_VAR);
 //   }  remove this parantheses too if you want this part of code
+
+
+
+// ====================================================
+// Your task is to write a new function that should be named combine and have the following characteristics:
+
+// Accept three input values
+
+// Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+// Return the calculated result
+// ====================================================
+
+            

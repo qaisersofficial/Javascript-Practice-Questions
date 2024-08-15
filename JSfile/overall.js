8058fa9d34a200420c0db32901f1e8ccfb90959c
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 12:11:06 +0500

    output of && ooperator

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -104,7 +104,7 @@
 
         // && operator
 
-        console.log( 0 && 0);
-        console.log( 0 && 1);
-        console.log( 1 && 0);
-        console.log( 1 && 1);
+        console.log( 0 && 0);   //0
+        console.log( 0 && 1);   //0
+        console.log( 1 && 0);   //0
+        console.log( 1 && 1);   //1

8d8875b7fef2bb0b247d787a16e67d4739882e4d
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 12:21:16 +0500

    Therefore, (A || B) and (A + B) are not equivalent in JavaScript.

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -126,5 +126,8 @@
         //  console.log( 1 * 1);   //1
 
         //(A + B)
-        
+        console.log( 0 + 0);   //0
+        console.log( 0 + 1);   //1
+        console.log( 1 + 0);   //1
+        console.log( 1 + 1);   //2
     

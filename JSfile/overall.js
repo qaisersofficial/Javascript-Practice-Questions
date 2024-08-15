5cff8351a202e6e44429254e43c9675e872c8957
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 11:18:05 +0500

    plus operator

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -1,7 +1,12 @@
-const name = "Qaiser Ali";
-const age =  "23";
-const city = "Gujrat";
+// const name = "Qaiser Ali";
+// const age =  "23";
+// const city = "Gujrat";
     // console.log(name);
     // console.log(age);
     // console.log(city);
-    console.log(`My name is ${name}, \nMy age is ${age}, \nMy city is ${city}.`)
+    // console.log(`My name is ${name}, \nMy age is ${age}, \nMy city is ${city}.`);
+
+// + operator
+
+    console.log( 10 + 5 )   //15
+    

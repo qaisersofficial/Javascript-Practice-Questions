b8cf805b09fb71696bfb7e6e61d13c0acd1ca350
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 11:46:55 +0500

    = with string

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -51,5 +51,6 @@
     //  console.log ( 10 = 5); // error
     // console.log ( 10 = 10); //error
     // console.log ( 10 == 5); //error
-    console.log ( 10 === 5); //false
+    // console.log ( 10 === 5); //false
+    console.log( "10" = "5")
     

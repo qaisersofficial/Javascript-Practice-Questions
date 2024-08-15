1249880770e3968d7e3526c2ac71d596252b9b7e
QAISER ALI <qaisers.official@gmail.com>
2024-08-15 12:10:25 +0500

    && ooperator

--- JSfile/overall.js
+++ JSfile/overall.js
@@ -97,7 +97,14 @@
   
         //    console.log( x ?? y);   //nothing will show in output
 
-        let x = undefined ;
-        let y ;
+        // let x = undefined ;
+        // let y ;
   
-           console.log( x ?? y);   //undefined
+        //    console.log( x ?? y);   //undefined
+
+        // && operator
+
+        console.log( 0 && 0);
+        console.log( 0 && 1);
+        console.log( 1 && 0);
+        console.log( 1 && 1);

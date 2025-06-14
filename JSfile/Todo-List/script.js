58dda744401315bf67a8c6176568f905b33c8cfc
QAISER ALI <qaisers.official@gmail.com>
2025-06-14 14:40:49 +0500

    Implement full-featured Todo List with Tailwind styling and delete functionality
    - Added input fields for task name and due date
    - Implemented addTodo() and renderTodoList() functions
    - Enabled task deletion with styled Tailwind button
    - Dynamically updated task heading based on number of todos
    - Styled input fields and buttons using Tailwind CSS
    - Handled Enter key press for adding todos

--- JSfile/Todo-List/script.js
+++ JSfile/Todo-List/script.js
@@ -6,8 +6,20 @@ function addTodo(){
     
     const inputElement = document.querySelector(".input1");
     const name = inputElement.value;
-    if(name){
-    todoArr.push(name);
+
+    const inputDate = document.querySelector("#inputDate");
+    const dueDate = inputDate.value;
+
+    if(name && dueDate){
+    todoArr.push(
+        {
+            // name: name,
+            // dueDate: dueDate
+            // shorthand proper
+            name, 
+            dueDate
+        }
+    );
     // console.log(todoArr);
     inputElement.value = "";
 
@@ -25,9 +37,30 @@ function renderTodoList(){
 let todoListHTML = '';
 
     for (let i = 0; i < todoArr.length; i++) {
-    const element = todoArr[i];
+    const elementObject = todoArr[i];
+    const { name, dueDate } = elementObject; //destructuring
     // console.log(element)
-    const paragraph = `<p>The value you entered: ${element}</p>`
+     const heading = document.getElementById("taskHeading");
+
+  if (todoArr.length === 1) {
+    heading.textContent = "Task Todo:";
+  } else if (todoArr.length > 1) {
+    heading.textContent = "Tasks Todo:";
+  } else  if (todoArr.length <= 0) {
+    heading.textContent = "No Tasks";
+  }
+    const paragraph = `
+    <li>
+        <span class="font-semibold text-gray-900 dark:text-white">${name}</span> on <span class="font-semibold text-gray-900 dark:text-white">${dueDate} </span>
+    <button
+      class="ml-2 px-3 py-1 bg-red-500 text-white text-sm rounded hover:bg-red-600 transition duration-200 cursor-pointer"
+    onclick= "
+    todoArr.splice(${i},1);
+    renderTodoList();
+    "
+    >Delete</button>
+    </li>
+   `
     todoListHTML += paragraph;
     
 }
